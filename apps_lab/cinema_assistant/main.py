a5a1c825a79e8b3e72946962b1801557c7013e46
DSmmartin <mjmm_manuel@hotmail.com>
2024-05-21 16:56:26 +0000

    add get_movie_details util

--- apps_lab/cinema_assistant/main.py
+++ apps_lab/cinema_assistant/main.py
@@ -1,7 +1,13 @@
 from loguru import logger
-from utils import get_id_movie
+from utils import get_id_movie, get_movie_details
 
 
 movie_name = "The Lord of the Rings The Fellowship of the Ring"
 movie_id = get_id_movie(movie_name)
 logger.info(f"Movie ID: {movie_id} from Movie Name: {movie_name}")
+
+logger.info(f"Getting movie details for movie ID: {movie_id}")
+movie_details = get_movie_details(movie_id)
+print(movie_details)
+
+logger.info("End of the program.")
--- apps_lab/cinema_assistant/utils.py
+++ apps_lab/cinema_assistant/utils.py
@@ -3,11 +3,22 @@ import json
 import requests
 
 API_THEMOVIE_BASE_URL = "https://api.themoviedb.org"
+API_THEMOVIE_VERSION = "3"
 PAGES = 1
 
+
 def get_id_movie(film_title: str):
+    """
+    Fetches and returns the ID of a movie from The Movie Database (TMDB).
+
+    :param film_title: The title of the movie.
+    :type film_title: str
+    :return: The ID of the movie.
+    :rtype: int
+    """
+
     parsed_title = film_title.replace(" ", "%20")
-    url = f"{API_THEMOVIE_BASE_URL}/3/search/movie?query={parsed_title}&include_adult=false&language=en-US&page={PAGES}"
+    url = f"{API_THEMOVIE_BASE_URL}/{API_THEMOVIE_VERSION}/search/movie?query={parsed_title}&include_adult=false&language=en-US&page={PAGES}"
 
     headers = {
         "accept": "application/json",
@@ -21,6 +32,42 @@ def get_id_movie(film_title: str):
     if response.status_code != 200:
         exit(f"Error: {response.status_code}")
     try:
-        return json.loads(response.text)['results'][0]['id']
+        return int(json.loads(response.text)['results'][0]['id'])
     except IndexError as e:
         return None
+
+
+def get_movie_details(movie_id: int):
+    """
+    Fetches and returns details of a movie from The Movie Database (TMDB).
+
+    :param movie_id: The ID of the movie.
+    :type movie_id: int
+    :return: A dictionary containing the movie details. The keys are 'original_title', 'genres', 'reviews', and 'release_date'.
+    :rtype: dict
+    """
+
+    url = f"{API_THEMOVIE_BASE_URL}/{API_THEMOVIE_VERSION}/movie/{str(movie_id)}?append_to_response=genre%2Creviews&language=en-US"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {os.environ.get('TMDB_BEARER_TOKEN')}"
+    }
+    try:
+        response = requests.get(url, headers=headers)
+    except requests.exceptions.RequestException as e:
+        exit(f"Error: {e}")
+
+    if response.status_code != 200:
+        exit(f"Error: {response.status_code}")
+
+    response = json.loads(response.text)
+    original_title = response['original_title']
+    genres = [genre['name'] for genre in response['genres']]
+    reviews = [review['content'] for review in response['reviews']['results']]
+
+    return {
+        "original_title": original_title,
+        "genres": genres,
+        "reviews": reviews,
+        "release_date": response['release_date']
+    }
